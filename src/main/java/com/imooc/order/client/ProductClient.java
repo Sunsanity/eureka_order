3cc07c61e496433f61aa243d2bbfcb076f85b4b3
sunjingwen <lin_sanity@aliyun.com>
2018-04-10 10:09:08 +0800

    订单服务提交

--- src/main/java/com/imooc/order/client/ProductClient.java
+++ src/main/java/com/imooc/order/client/ProductClient.java
@@ -1,5 +1,6 @@
 package com.imooc.order.client;
 import com.imooc.order.dataobject.ProductInfo;
+import com.imooc.order.dto.CartDTO;
 import org.springframework.cloud.netflix.feign.FeignClient;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PostMapping;
@@ -7,18 +8,13 @@ import org.springframework.web.bind.annotation.RequestBody;
 
 import java.util.List;
 
-/**
- * Created by 廖师兄
- * 2017-12-10 21:04
- */
-//@FeignClient(name = "product")
+@FeignClient(name = "product")
 public interface ProductClient {
 
-//    @GetMapping("/msg")
-//    String productMsg();
-//
-//    @PostMapping("/product/listForOrder")
-//    List<ProductInfo> listForOrder(@RequestBody List<String> productIdList);
+    @PostMapping("/product/listForOrder")
+    List<ProductInfo> listForOrder(@RequestBody List<String> productIdList);
+
+    @PostMapping("/product/decreaseStock")
+    void decreaseStock(@RequestBody List<CartDTO> cartDTOList);
 
 }
-
